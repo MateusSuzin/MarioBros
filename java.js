ef386d5c372be4ae3fdb5b32ff372c5729065a65
MateusSuzin <mateus_s_pertile@estudante.sesisenai.org.br>
2026-05-18 15:50:55 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,49 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+/* ======================
+   PULO
+====================== */
+
+const jump = () => {
+
+    // evita spam de pulo
+    if (mario.classList.contains('jump')) return;
+
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
+document.addEventListener('keydown', jump);
+
+
+/* ======================
+   COLISÃO
+====================== */
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+
+    const marioPosition =(+window.getComputedStyle(mario).bottom.replace('px','')
+        );
+
+    if (pipePosition <= 110 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'imagens/game-over.png';
+
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }
+
+}, 10);
